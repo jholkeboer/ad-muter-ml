03fbb2455e5ba8f0c389bb7a2444a40f76dd07dd
jholkeboer <jholkeboer@users.noreply.github.com>
2016-12-23 15:22:46 -0800

    parsing json

--- project.clj
+++ project.clj
@@ -1,9 +1,12 @@
 (defproject ad-muter-ml "0.1.0-SNAPSHOT"
-  :description "FIXME: write description"
-  :url "http://example.com/FIXME"
+  :description "Image Machine Learning"
+  :url "https://github.com/jholkeboer/ad-muter-ml"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.8.0"]]
+  :dependencies [[org.clojure/clojure "1.8.0"]
+                 [ring "1.4.0"]
+                 [compojure "1.5.0"]
+                 [hiccup "1.0.5"]]
   :main ^:skip-aot ad-muter-ml.core
   :target-path "target/%s"
   :profiles {:uberjar {:aot :all}})
--- src/ad_muter_ml/core.clj
+++ src/ad_muter_ml/core.clj
@@ -1,7 +1,29 @@
 (ns ad-muter-ml.core
-  (:gen-class))
+  (:gen-class)
+  (:use hiccup.core)
+  (:require
+    [ring.adapter.jetty :as jetty]
+    [ring.middleware.params :only [wrap-params]]
+    [compojure.core :refer :all]
+    [compojure.route :as route]))
+
+(defroutes app-routes
+  (GET "/test_page" [] "Testpage.")
+  ; (POST "/receive_image" [time] (do (println (str "Got time " time))) "Got request")
+  (POST "/receive_image" request
+    (do
+      (println (str "Got params? " (:body request)))
+      (let [body (ring.util.request/body-string request)]
+        (println body))
+      "Got request"))
+  (route/not-found "404 Route not found."))
+
+(def app (ring.middleware.params/wrap-params app-routes))
 
 (defn -main
   "I don't do a whole lot ... yet."
   [& args]
-  (println "Hello, World!"))
+  (let [port (Integer. (get (System/getenv) "AD_MUTER_PORT" "5000"))]
+    (jetty/run-jetty
+      app
+      {:port port})))
