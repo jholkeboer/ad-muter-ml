997ae84a752d39b46a375e809ec2701da2bea6f9
jholkeboer <jholkeboer@users.noreply.github.com>
2016-12-24 13:19:37 -0800

    writing legible pngs

--- project.clj
+++ project.clj
@@ -4,10 +4,11 @@
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.8.0"]
+                 [org.clojure/data.json "0.2.6"]
+                 [org.clojure/data.codec "0.1.0"]
                  [ring "1.4.0"]
                  [compojure "1.5.0"]
-                 [hiccup "1.0.5"]
-                 [org.clojure/data.json "0.2.6"]]
+                 [hiccup "1.0.5"]]
   :main ^:skip-aot ad-muter-ml.core
   :target-path "target/%s"
   :profiles {:uberjar {:aot :all}})
--- src/ad_muter_ml/core.clj
+++ src/ad_muter_ml/core.clj
@@ -6,23 +6,32 @@
     [ring.middleware.params :only [wrap-params]]
     [compojure.core :refer :all]
     [compojure.route :as route]
-    [clojure.data.json :as json]))
+    [clojure.data.json :as json]
+    [clojure.java.io :as io]
+    [clojure.data.codec.base64 :as b64-codec]))
+
+(defn write-image-to-file
+  "Takes base64 string"
+  [image-data time]
+  (let [decoded-data (-> image-data
+                      (clojure.string/split #"base64,")
+                      (second)
+                      (.getBytes)
+                      (b64-codec/decode))
+        path    (str "images/" time  ".png")]
+    (io/copy decoded-data (java.io.File. path))))
 
 (defroutes app-routes
-
   ;; Lighteight page for testing (small file size)
   (GET "/test_page" [] "Testpage.")
 
   ;; Receive an image from the chrome extension
   (POST "/receive_image" request
     (let [raw-body (json/read-str (ring.util.request/body-string request))
-          image-time (get raw-body "time")
-          image-data (get raw-body "image")
-          path  (str "images/" image-time ".png")]
-      (println (get raw-body "time"))
+          time (get raw-body "time")
+          image-data (get raw-body "image")]
       (println (apply str (take 20 image-data)))
-      (spit path image-data)
-      (println (str "Saved " path))
+      (write-image-to-file image-data time)
       "Saved image."))
 
   (route/not-found "404 Route not found."))
